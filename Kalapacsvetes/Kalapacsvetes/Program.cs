762c3bb65152eb718671409b86fefa7374ce7d77
unknown <szakacs.tamas.21@ady-nagyatad.hu>
2025-09-05 13:08:23 +0200

    dgsdg

---
+++ Kalapacsvetes/Kalapacsvetes/Program.cs
@@ -0,0 +1,60 @@
+﻿using Kalapacsvetes;
+
+
+string[] olvas = File.ReadAllLines("kalapacsvetes.txt");
+
+List<Sportolo> sportolok = new List<Sportolo>();
+
+foreach (string line in olvas.Skip(1))
+{
+    sportolok.Add(new Sportolo(line));
+}
+
+Console.WriteLine("4. feladat: {0} dobás eredménye található.", sportolok.Count);
+
+double sum = 0;
+int db = 0;
+
+foreach (Sportolo e in sportolok)
+{
+    if (e.orszagkod == "HUN")
+    {
+        sum += e.eredmeny;
+        db++;
+    }
+}
+
+Console.WriteLine("5. feladat: A magyar sportolók átlagosan {0} métert dobtak.", sum / db);
+
+
+Console.WriteLine("6. feladat: Adjon meg egy évszámot:");
+
+string input = Console.ReadLine();
+
+int darab = 0;
+
+foreach (Sportolo e in sportolok)
+{
+    if (e.datum.Substring(0, 4) == input)
+    {
+
+        darab++;
+    }
+}
+if (darab != 0)
+{
+    Console.WriteLine("{0} darab dobás került be ebben az évben.", darab);
+    foreach (Sportolo e in sportolok)
+    {
+        if (e.datum.Substring(0, 4) == input)
+        {
+
+            Console.WriteLine(e.sportolo);
+        }
+    }
+}
+else 
+{
+   
+}
+    
---
+++ Kalapacsvetes/Kalapacsvetes/Sportolo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    internal class Sportolo
+    {
+        public int helyezes;
+        public double eredmeny;
+        public string sportolo;
+        public string orszagkod;
+        public string helyszin;
+        public string datum;
+
+        public Sportolo(string sor)
+        {
+            string[] vag = sor.Split(";");
+            this.helyezes = int.Parse(vag[0]);
+            this.eredmeny = double.Parse(vag[1]);
+            this.sportolo = vag[2];
+            this.orszagkod = vag[3];
+            this.helyszin = vag[4];
+            this.datum = vag[5];
+        }
+    }
+}
