506cab1f72bc61c0e34742de223449f96968b622
unknown <szakacs.tamas.21@ady-nagyatad.hu>
2025-09-14 08:28:58 +0200

    fsdg

---
+++ Fuggohidak/Fuggohidak/Fuggohid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuggohidak
+{
+    internal class Fuggohid
+    {
+        public int helyezes;
+        public string nev;
+        public string fhely;
+        public string orszag;
+        public int hossz;
+        public int ev;
+
+        public Fuggohid(string sor)
+        {
+            string[] vag = sor.Split('\t');
+            helyezes = int.Parse(vag[0]);
+            nev = vag[1];
+            fhely = vag[2];
+            orszag = vag[3];
+            hossz = int.Parse(vag[4]);
+            ev = int.Parse(vag[5]);
+        }
+
+
+    }
+}
---
+++ Fuggohidak/Fuggohidak/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fuggohidak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Fuggohid> adatok = new List<Fuggohid>();
+            string[] line = File.ReadAllLines("fuggohidak.csv");
+            foreach (string adat in line.Skip(1))
+            {
+                adatok.Add(new Fuggohid(adat));
+            }
+            foreach (Fuggohid hid in adatok)
+            {
+                liszt.Items.Add(hid.nev);
+              
+            }
+        }
+    
+    }
+}
